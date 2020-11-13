068e3066af937cde04aa91c085a9f64b5f9d1d06
Ilan Francisco da Silva <i257199@dac.unicamp.br>
2020-11-13 15:07:24 -0300

    Adicionado gráfico e melhorias na interface

---
+++ src/init_chart.js
@@ -0,0 +1,67 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart;
+
+function create_chart() {
+    if (chart) {
+        chart.destroy();
+    }
+    chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            labels: [],
+            datasets: [{
+                label: 'Agulhas que cruzaram a linha',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: []
+            }, {
+                label: 'Total de agulhas',
+                backgroundColor: 'rgb(189,52,189)',
+                borderColor: 'rgb(189,52,189)',
+                data: []
+            }]
+        },
+
+        // Configuration options go here
+        options: {
+            scales: {
+                yAxes: [{
+                    position: "left",
+                    ticks: {
+                        beginAtZero: true
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Quantidade de dados'
+                    },
+                    id: "y-axis-0"
+                }, {
+                    position: "right",
+                    ticks: {
+                        min: 0,
+                        max: 3.14,
+                        stepSize: 0.5,
+                        precision: 2,
+                        autoSkip: true,
+                        callback: function(value, index, values) {
+                            return  value.toFixed(2);
+                        }
+                    },
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Proporção para π'
+                    },
+                    max: 123,
+                    id: "y-axis-1"
+                }]
+            }
+        },
+        animation: {
+            duration: 0
+        }
+
+    });
+}
---
+++ src/simulator.js
@@ -0,0 +1,81 @@
+const height = $("#canvas").height();
+const width = $("#canvas").width();
+const NEEDLE_HEIGHT = 66;
+const NEEDLE_WIDTH = 2;
+const used_width = width - width % (2*NEEDLE_HEIGHT);
+const needle_img = document.getElementById("needle-img");
+
+var c = document.getElementById("canvas");
+c.height = height;
+c.width = used_width;
+
+function radians(degrees) {
+    return degrees * (Math.PI/180);
+}
+
+function generate_result(ctx) {
+    const x = Math.floor( Math.random() * used_width );
+    const y = Math.floor( Math.random() * height );
+    const angle = Math.floor( Math.random() * 180) - 90;
+
+    const needle_left_x  = x - Math.cos(radians(angle)) * NEEDLE_HEIGHT/2;
+    const needle_right_x = x + Math.cos(radians(angle)) * NEEDLE_HEIGHT/2;
+
+    ctx.save();
+    ctx.translate(x,y);
+    ctx.rotate(radians(angle-90));
+    ctx.translate(-NEEDLE_WIDTH/2,-NEEDLE_HEIGHT/2);
+    ctx.drawImage(needle_img, 0,0, NEEDLE_WIDTH, NEEDLE_HEIGHT);
+    ctx.restore();
+
+
+    let i;
+    for (i = 0; i <= used_width; i += 2*NEEDLE_HEIGHT) {
+        const line_x = i;
+        if (line_x > needle_left_x &&
+            line_x < needle_right_x ) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+function generate_sample_result(n_sample) {
+    var ctx = c.getContext("2d");
+    ctx.clearRect(0,0,width,height);
+    ctx.beginPath();
+    ctx.save();
+    ctx.lineWidth = 1;
+
+    let i;
+    for (i = 0; i <= used_width; i+=2*NEEDLE_HEIGHT) {
+        ctx.moveTo(i, 0);
+        ctx.lineTo(i, height);
+        ctx.stroke();
+    }
+
+    let count_crosseds = 0;
+    i = 0;
+    function step() {
+        if (i < n_sample) {
+            count_crosseds += generate_result(ctx);
+            chart.data.labels.push(++i);
+            chart.data.datasets[1].data.push(i);
+            chart.data.datasets[0].data.push(count_crosseds);
+            chart.options.scales.yAxes[0].ticks.max = i;
+            chart.options.scales.yAxes[1].ticks.max = i / count_crosseds;
+            chart.update();
+            $("#pi_result").text((i / count_crosseds).toFixed(2));
+            setTimeout(step, 50);
+        }
+    }
+
+    step();
+}
+
+$("#btn-go").click( function () {
+    create_chart();
+    $("#pi_result_container")[0].hidden = false;
+    let n_sample = parseInt($("#n_sample").val());
+    generate_sample_result(n_sample);
+})
